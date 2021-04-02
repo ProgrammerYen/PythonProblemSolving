d5b871fee63f5295ca3938b6afd62e53aede440d
Kaushallya <kaushallyadealwis@yahoo.co.uk>
2021-04-02 11:18:54 +0100

    New file added for factorial trailing zeros

---
+++ trailing-zeros.py
@@ -0,0 +1,12 @@
+def trailing_zeros(n):
+	num_count = 0
+	num_power = 2
+	num_count += n // 5
+
+	while n // 5 ** num_power >= 1:
+		num_count += n // 5 ** num_power
+		num_power += 1
+
+	return num_count
+
+print(trailing_zeros(30))
