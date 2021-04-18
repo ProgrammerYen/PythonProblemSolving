2daffd487339229ff3dacc8a49ee385782320451
Kaushallya <kaushallyadealwis@yahoo.co.uk>
2021-04-18 17:14:12 +0100

    Added new files called beeramid

---
+++ beeramid.py
@@ -0,0 +1,11 @@
+def beeramid(bonus, price):
+    number = 1
+    num_count = 0
+    while bonus >= (number ** 2) * price:
+        num_count += 1
+        bonus -= (number ** 2) * price
+        number += 1
+        
+    return num_count
+
+print(beeramid(23098, 23))
