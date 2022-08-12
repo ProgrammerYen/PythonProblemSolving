51276b4f90b8caa75528b5e1dfb6ea21a28ef818
Yenula De Alwis <dealwisyenula@gmail.com>
2022-08-12 12:37:43 +0100

    Renamed files

--- pascalSnake.py
+++
@@ -1,9 +0,0 @@
-import re
-def to_underscore(string):
-    # your code here
-    try:
-        int(string)
-        return str(string)
-    
-    except:
-        return "_".join([i.lower() for i in re.findall('[A-Z][a-z]*[0-9]*', string)])
--- pythonProbCheck.py
+++
@@ -1,8 +0,0 @@
-def namelist(names):
-    if len(names) == 0: 
-        return ""
-    
-    formatted = [i["name"] + "," for i in names]
-    print(*formatted[:-2], formatted[-2][:-1], "&", formatted[-1][:-1])
-
-namelist([{'name': 'Bart'},{'name': 'Lisa'},{'name': 'Maggie'},{'name': 'Homer'},{'name': 'Marge'}])
